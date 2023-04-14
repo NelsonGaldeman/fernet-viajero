41ef26524ffd98a62545bce2f97e7bc65bac074d
Nelson Galdeman <nelsongaldeman@gmail.com>
2023-04-14 18:06:08 +0100

    fix: subgraph update

--- packages/subgraph/src/mappings/viajero.ts
+++ packages/subgraph/src/mappings/viajero.ts
@@ -1,6 +1,6 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { ERC721, Transfer } from '../../generated/Viajero/ERC721';
-import { Current, Previous } from '../../generated/schema';
+import { Current, PreviousHolder } from '../../generated/schema';
 
 export const VIAJERO_TOKEN_ID = BigInt.fromString('21260');
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
@@ -20,14 +20,14 @@ export function handleTransfer(event: Transfer): void {
     current = new Current('1');
     current.block = BigInt.fromString('41495857'); //nft minting block timestamp
     current.timestamp = BigInt.fromString('1660491043');
+  } else {
+    let previous = new PreviousHolder(event.block.number.toHexString());
+    previous.address = event.params.from;
+    previous.block = current.block;
+    previous.timestamp = current.timestamp;
+    previous.tiempo = event.block.timestamp.minus(current.timestamp);
+    previous.save();
   }
-
-  let previous = new Previous(event.block.number.toHexString());
-  previous.address = event.params.from;
-  previous.block = current.block;
-  previous.timestamp = current.timestamp;
-  previous.tiempo = event.block.timestamp.minus(current.timestamp);
-  previous.save();
 
   current.address = event.params.to;
   current.block = event.block.number;
